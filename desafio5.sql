409df409a089136b16095d07f01d54304cca48a1
Thales Lima <thales.bplima@gmail.com>
2022-01-26 23:41:15 -0300

    impl req5

---
+++ desafio5.sql
@@ -0,0 +1,8 @@
+SELECT songs.songs_name AS 'cancao', COUNT(hist.id_songs) AS 'reproducoes'
+FROM
+	SpotifyClone.user_history AS hist
+    INNER JOIN
+    SpotifyClone.songs AS songs ON hist.id_songs = songs.songs_id
+GROUP BY id_songs
+ORDER BY reproducoes DESC, cancao
+LIMIT 2;
